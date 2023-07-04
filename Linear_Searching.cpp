56b5e1d0b9965b5a8625d2ad84ef1f3997a39095
prernapriya <38802922+k1653@users.noreply.github.com>
2023-07-04 22:29:37 +0530

    Create Linear_Searching.cpp

---
+++ Linear_Searching.cpp
@@ -0,0 +1,13 @@
+// Linear Searching
+#include <iostream>
+using namespace std;
+
+void linearSearch(int arr[], int size, int target){
+  // linear searching algorithm
+}
+
+int main(){
+
+
+return 0;
+}
