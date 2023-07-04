4f92d9981741cde548efecf2816a035b28270db5
prernapriya <38802922+k1653@users.noreply.github.com>
2023-07-04 22:38:55 +0530

    Create Binary_Search.cpp

---
+++ Binary_Search.cpp
@@ -0,0 +1,18 @@
+// Binary Search
+
+#include <iostream>
+using namespace std;
+
+void binarySearch(int list[], int size, int target){
+  // Binary search here
+
+  
+}
+
+int main(){
+  int list[] = {2,23,53,4,5,7,823,34}; 
+  
+  binarySearch(list);
+ 
+  return 0;
+}
